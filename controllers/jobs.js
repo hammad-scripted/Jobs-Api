c407b431fa27071ff1c082421c76133ee6d9a5fa
Mohammad Hammad <hammad.scripted@gmail.com>
2026-05-15 15:14:37 +0530

    feat: add authentication and job management routes with controller integration

---
+++ controllers/jobs.js
@@ -0,0 +1,8 @@
+export const getAllJobs = async (req, res) => res.send('register user');
+
+export const getJob = async (req, res) => res.send('login user');
+export const createJob = async (req, res) => res.send('register user');
+
+export const updateJob = async (req, res) => res.send('login user');
+
+export const deleteJob = async (req, res) => res.send('register user');
---
+++ routes/jobs.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const router = express.Router();
+import {
+  getAllJobs,
+  getJob,
+  updateJob,
+  deleteJob,
+} from '../controllers/jobs.js';
+
+router.get('/', getAllJobs);
+router.post('/', createJob);
+router.get('/:id', getJob);
+router.put('/', updateJob);
+router.delete('/:id', deleteJob);
+
+export default router;
