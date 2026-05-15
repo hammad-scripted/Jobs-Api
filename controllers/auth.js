47a8d16942de453de8af6d96fc108e6bb63ec7c5
Mohammad Hammad <hammad.scripted@gmail.com>
2026-05-15 16:05:42 +0530

    chore: update dependencies and add VSCode settings
    - Added bcrypt version 6.0.0 to dependencies for improved security.
    - Updated mongoose from version 5.13.2 to 9.6.2 for better performance and features.
    - Created a new VSCode settings file to include custom spell check words.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,24 @@
-export const register = async (req, res) => res.send('register user');
+import { StatusCodes } from 'http-status-codes';
+import User from '../models/User.js';
+import BadRequestError from '../errors/bad-request.js';
+import bcrypt from 'bcrypt';
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    throw new BadRequestError('Please provide all values');
+  }
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new BadRequestError('Email already exists');
+  }
+  //   hash password
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({ name, email, password: hashedPassword });
+  return res.status(StatusCodes.CREATED).json({ user });
+};
 
 export const login = async (req, res) => res.send('login user');
---
+++ models/User.js
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide a valid email',
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+  },
+});
+
+const User = model('User', userSchema);
+export default User;
